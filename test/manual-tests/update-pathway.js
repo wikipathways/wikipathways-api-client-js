d56fd94d143b1c6e2d5d657ab14480460e60c970
Anders Riutta <git@andersriutta.com>
2015-06-15 16:42:09 -0700

    Test hyperquest method.

--- test/manual-tests/update-pathway.js
+++ test/manual-tests/update-pathway.js
@@ -2,12 +2,13 @@ var fs = require('fs');
 var WikipathwaysApiClient = require('../../index.js');
 
 var wikipathwaysApiClientInstance = new WikipathwaysApiClient({
-  baseIri: 'http://webservice.wikipathways.org/'
+  baseIri: 'http://pvjs.wikipathways.org/wpi/webservicetest/'
+  //baseIri: 'http://webservice.wikipathways.org/'
 });
 
 wikipathwaysApiClientInstance.updatePathway({
   identifier: 'WP4',
-  version: 0,
+  version: 80313,
   gpml: fs.readFileSync('../input-data/test.gpml').toString(),
   description: 'Testing update from wikipathways-api-client-js'
 })
