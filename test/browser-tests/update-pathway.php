fba7a39669d9719fb2e1b1c54e1a7ac1f13f0f08
Anders Riutta <ariutta@users.noreply.github.com>
2016-01-28 12:29:03 -0800

    Update update-pathway.php

--- test/browser-tests/update-pathway.php
+++ test/browser-tests/update-pathway.php
@@ -6,7 +6,7 @@
     <?php echo 'window.wikipathwaysToken = ' . getTokenSomehowHere() . ';'; ?>
   <?php echo '</script>'; ?>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-    <script src="./dist/wikipathways-api-client-polyfills-1.0.5.bundle.min.js"></script>
+    <script src="../../dist/wikipathways-api-client-polyfills-1.0.5.bundle.min.js"></script>
     <script src="../../dist/wikipathways-api-client.dev.js"></script>
   </head>
 
