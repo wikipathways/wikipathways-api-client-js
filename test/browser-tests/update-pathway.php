74930743ea8c3a7dd157ae9c6f8f07de3cdbaf7b
Anders Riutta <git@andersriutta.com>
2016-01-28 19:02:11 -0800

    Update to make it work on wikipathways.org

--- test/browser-tests/update-pathway.php
+++ test/browser-tests/update-pathway.php
@@ -1,9 +1,44 @@
+<?php 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+chdir(dirname(realpath(__FILE__)) . "/../../../../");
+// loads WikiPathways environment variables
+require_once('wpi.php');
+
+//var_dump($wgUser);
+
+function getUsername(){
+        global $wgUser;
+        return $wgUser->mName;
+}
+
+function getToken(){
+        global $wgUser;
+        return $wgUser->mToken;
+}
+
+// TODO
+function getNextPathwayVersion(){
+        return 0;
+}
+
+
+//require_once('WSFault.php');
+//require_once('search.php');
+//require_once('extensions/otag/OntologyFunctions.php');
+chdir($dir);
+?>
+
+
 <html>
 
   <head>
     <?php echo '<script type="text/javascript">'; ?>
-      <?php echo 'window.wikipathwaysUsername = "' . getUsernameSomehowHere() . '";'; ?>
-      <?php echo 'window.wikipathwaysToken = "' . getTokenSomehowHere() . '";'; ?>
+      <?php echo 'window.wikipathwaysUsername = "' . getUsername() . '";'; ?>
+      <?php echo 'window.wikipathwaysToken = "' . getToken() . '";'; ?>
+      <?php echo 'window.nextPathwayVersion = "' . getNextPathwayVersion() . '";'; ?>
     <?php echo '</script>'; ?>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
     <script src="../../dist/wikipathways-api-client-polyfills-1.0.5.bundle.min.js"></script>
@@ -21,13 +56,13 @@
       var wikipathwaysApiClientInstance = new WikipathwaysApiClient();
 
       $('#update-btn').click(function() {
-        var gpml = $('#pathway').val();
+        var gpml = $('#gpml').val();
         wikipathwaysApiClientInstance.updatePathway({
           identifier: 'WP4',
-          version: 0,
+          version: nextPathwayVersion,
           gpml: gpml,
           description: 'Testing update from wikipathways-api-client-js in browser',
-          username: username,
+          username: wikipathwaysUsername,
           auth: wikipathwaysToken
         })
         .then(function(response) {
